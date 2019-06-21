a942d374a5e7a9757153a593fbe1fd15c34c9dbb
Denis Manente <denis.manente@automatic-server.com>
2019-06-21 17:08:55 +0200

    add missing endpoint address to streaming address

--- cri/server.go
+++ cri/server.go
@@ -89,7 +89,7 @@ func NewServer(criConfig *LXEConfig) *Server {
 	}
 
 	// for now we bind the http on every interface
-	streamServerAddr := ":" + criConfig.LXEStreamingPort
+	streamServerAddr := criConfig.LXEStreamingServerEndpoint + ":" + criConfig.LXEStreamingPort
 	runtimeServer, err := NewRuntimeServer(criConfig, streamServerAddr, lxf)
 	if err != nil {
 		logger.Critf("Unable to start runtime server: %v", err)

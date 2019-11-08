06e8635d2d3ffa16d9726fc0456f1f5ec0e098d9
Denis Manente <denis.manente@automatic-server.com>
2019-11-08 18:55:42 +0100

    remove duplicate commented code

--- lxf/exec_test.go
+++ lxf/exec_test.go
@@ -41,56 +41,6 @@ func TestClient_Exec_BasicOk(t *testing.T) {
 	assert.Equal(t, int32(8), exitCode)
 }
 
-// func TestConnection(t *testing.T) {
-// 	_, err := lxf.NewClient("", os.Getenv("HOME")+"/.config/lxc/config.yml")
-// 	if err != nil {
-// 		t.Errorf("failed to set up connection %v", err)
-// 	}
-// }
-
-// func TestConnectionWithInvalidSocket(t *testing.T) {
-// 	_, err := lxf.NewClient("/var/lib/lxd/unix.invalidsocket",
-// 		os.Getenv("HOME")+"/.config/lxc/config.yml")
-// 	if err == nil {
-// 		t.Errorf("invalid socket should return an error")
-// 	}
-// }
-
-// func NewTestClient(t *testing.T) *Client {
-// 	client, err := NewClient("", os.Getenv("HOME")+"/.config/lxc/config.yml")
-// 	assert.NoError(t, err)
-// 	return client
-// }
-
-// // clear tries to remove all resources to provide a clean test base
-// func (lt *lxfTest) clear() {
-// 	for _, ct := range lt.listContainers() {
-// 		lt.stopContainer(ct.ID)
-// 		lt.deleteContainer(ct.ID)
-// 	}
-// 	for _, sb := range lt.listSandboxes() {
-// 		lt.stopSandbox(sb.ID)
-// 		lt.deleteSandbox(sb.ID)
-// 	}
-
-// 	// remove all image aliases except the busybox one
-// 	for _, im := range lt.listImages("") {
-// 		for _, al := range im.Aliases {
-// 			keep := false
-// 			for _, k := range keepImages {
-// 				if k == al {
-// 					keep = true
-// 					continue
-// 				}
-// 			}
-// 			if !keep {
-// 				fmt.Println("remove image", al)
-// 				lt.removeImage(al)
-// 			}
-// 		}
-// 	}
-// }
-
 // func TestExecSyncInParallel(t *testing.T) {
 // 	lt := newLXFTest(t)
 // 	lt.createContainer(&lxf.Container{
